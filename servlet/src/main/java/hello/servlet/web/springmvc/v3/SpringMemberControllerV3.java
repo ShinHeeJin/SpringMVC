4f9409602d9f093c58cfd8fe630c2cc72c72d44c
ShinHeeJin <sisesu92@gmail.com>
2023-08-07 01:15:17 +0900

    Http Method Definition 2

--- servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,9 +4,7 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,12 +13,12 @@ import java.util.List;
 public class SpringMemberControllerV3 {
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping(value = "/new-form")
     public String process() {
         return "new-form";
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping(value = "/save")
     public String save(@RequestParam("username") String username, @RequestParam("age") int age, Model model) {
         Member member = new Member(username, age);
         memberRepository.save(member);
@@ -28,7 +26,7 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String getMembers(Model model) {
         List<Member> members = memberRepository.findAll();
         model.addAttribute("members", members);
