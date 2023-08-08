32c6d2cce2acbaf29ef8b117827c408028e98d90
ShinHeeJin <sisesu92@gmail.com>
2023-08-09 01:08:52 +0900

    ItemRepositoryTest

---
+++ iterm-service/src/test/java/hello/itermservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,65 @@
+package hello.itermservice.domain.item;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearStore();
+    }
+
+    @Test // Junit 5부터 public 필요 없음
+    void save() {
+
+        Item item = new Item("itemA", 10000, 10);
+
+        Item savedItem = itemRepository.save(item);
+
+        Item findItem = itemRepository.findById(item.getId());
+
+        assertThat(findItem).isEqualTo(savedItem);
+
+    }
+
+    @Test // Junit 5부터 public 필요 없음
+    void findAll() {
+
+        Item item1 = new Item("item1", 10000, 10);
+        Item item2 = new Item("item2", 20000, 20);
+
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        List<Item> result = itemRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(item1, item2);
+
+    }
+
+    @Test // Junit 5부터 public 필요 없음
+    void updateItem() {
+        Item item = new Item("item1", 10000, 10);
+
+        Item savedItem = itemRepository.save(item);
+        Long itemId = savedItem.getId();
+
+        Item updateParam = new Item("item2", 20000, 30);
+        itemRepository.update(itemId, updateParam);
+
+        Item findItem = itemRepository.findById(itemId);
+
+        assertThat(findItem.getItermNam()).isEqualTo(updateParam.getItermNam());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+    }
+}
